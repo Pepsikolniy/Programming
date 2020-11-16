a2df7374291067b37b844b8e4e1bd6cf2516529a
Pepsikolniy <9876trewq@mail.ru>
2020-11-16 13:55:20 +0300

    24 neponyatno

---
+++ Practice/24/C++/24/24,1/24,1.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<fstream>
+#include"json.hpp"
+#include<iomanip>
+
+using json = nlohmann::json;
+
+int main()
+{
+	std::ifstream input("in.json");
+	std::ofstream output("out.json");
+
+	json jin;
+	input >> jin;
+	
+	json jout;
+
+	????
+	????
+	????
+	????
+	????
+
+	output << std::setw(2) << jout << std::endl;
+}
